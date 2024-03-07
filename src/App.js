7f3b955441fcf83dbf77120c5ab077da2f448d56
Mikita1012 <mikitashah2009@gmail.com>
2024-03-07 13:34:00 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Phone Directory</h1>
-    </div>
+    <Fragment>
+      <div>
+        <h1>Phone Directory</h1>
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span> <br />
+        <span>Phone</span>
+      </div>
+      <img src="../images/upgrad_logo.png"/>
+      <img src="../images/upgrad_logo.png"></img>
+      <label htmlFor="name">Name: </label>
+      <input type='text' id='name' placeholder='Type Here' defaultValue="Mikita"></input>
+      <label htmlFor="phone">Phone: </label>
+      <input type='tel' id='phone' placeholder='Type Here' defaultValue="1234567890"></input>
+    </Fragment>
   );
 }
 
