9b01a5ac9181da1dc6abcfdd2bda7c1d4ef54511
Mikita1012 <mikitashah2009@gmail.com>
2024-03-08 17:57:32 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
+import Header from './Header';
 
-function App() {
-  return (
-    <Fragment>
-      <div>
-        <h1>Phone Directory</h1>
-      </div>
-      <button>Add</button>
-      <div>
-        <span>Name</span> <br />
-        <span>Phone</span>
-      </div>
-      <img src="../images/upgrad_logo.png"/>
-      <img src="../images/upgrad_logo.png"></img>
-      <label htmlFor="name">Name: </label>
-      <input type='text' id='name' placeholder='Type Here' defaultValue="Mikita"></input>
-      <label htmlFor="phone">Phone: </label>
-      <input type='tel' id='phone' placeholder='Type Here' defaultValue="1234567890"></input>
-    </Fragment>
-  );
+class App extends Component {
+  render() {
+    let firstname = "Mikita";
+    let lastname = "Shah";
+    let numberOfIceCreams = 3
+
+    let iceCreamPrice = 20;
+    return (
+      <Fragment>
+        {/* <span>Hello {firstname} {lastname}! The total amount you need to pay for {numberOfIceCreams} is Rs.{numberOfIceCreams * iceCreamPrice} </span> */}
+        <Header />
+        <button>Add</button>
+        <div>
+          <span>Name</span> <br />
+          <span>Phone</span>
+        </div>
+        <label htmlFor="name">Name: </label>
+        <input type='text' id='name' placeholder='Type Here' defaultValue="Mikita"></input>
+        <label htmlFor="phone">Phone: </label>
+        <input type='tel' id='phone' placeholder='Type Here' defaultValue="1234567890"></input>
+        
+      </Fragment>
+    );
+  }
+
 }
 
 export default App;
