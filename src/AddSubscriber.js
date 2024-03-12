5be00946f5e2abb2dbc811607bae3f263ff14d48
Mikita1012 <mikitashah2009@gmail.com>
2024-03-12 16:00:30 +0530

    Added common folder having common.css in it. Modified index.js with adding css file

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import './AddSubscriber.css'
-import './App.css'
+// import './App.css'
 
 export default class AddSubscriber extends Component {
     render() {
@@ -16,8 +16,8 @@ export default class AddSubscriber extends Component {
                         <label htmlFor="phone" className="label-control">Phone: </label> <br />
                         <input type='tel' id='phone' className="input-control"></input> <br /> <br />
                         <div className='subscriber-info-container'>
-                            <span className='subscriber-to-add-heading'>Sunscriber to be added</span>
-                            <span className='subscriber-info'>Name: </span>
+                            <span className='subscriber-to-add-heading'>Subscriber to be added</span> <br/>
+                            <span className='subscriber-info'>Name: </span> <br/>
                             <span className='subscriber-info'>Phone: </span>
                         </div>
                         <button className='custom-btn add-btn'>ADD</button>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AddSubscriber from './AddSubscriber';
+import './common/common.css'
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
