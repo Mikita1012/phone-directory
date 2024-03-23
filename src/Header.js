9b28c397d7ad52eacb6275b8cf7a1fd1047267f1
Mikita1012 <mikitashah2009@gmail.com>
2024-03-23 20:46:57 +0530

    Added a custom hook for location, which is a part of Header component

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './Header.css'
+import { useLocationDetail } from './customizedHook';
+
+
+
 
 const Header = function (props) {
+
+    const locationDetail = useLocationDetail();
+    const { city, region, country_name } = locationDetail;
+
     return (
         <>
             <div className='header'>
                 {props.heading}
             </div>
+            <h4 style={{textAlign: 'center'}}> Welcome user. You location is - {city} from {region} in {country_name}</h4>
         </>
     );
 }
